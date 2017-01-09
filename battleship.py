a80b761dda213b2fe89af93527fb0dd55f3fc05e
Julieta <julieta.gomez.frittelli@gmail.com>
2017-01-08 20:54:09 -0800

    add player turns and draw board from data

--- battleship.py
+++ battleship.py
@@ -3,17 +3,33 @@
 import keyboardInput
 import display
 
+emptyBoard = [ [0] * 10 for _ in range(10)]
+
+playerOneTopBoard = emptyBoard
+playerOneBottomBoard = emptyBoard
+playerTwoTopBoard = emptyBoard
+playerTwoBottomBoard = emptyBoard
+
 def updateData(keystroke):
 	pass
 
 if __name__ == "__main__":
 	# every loop in main game loop will refresh the screen 
 	# or get keyboard input
+	turnCounter = 1
+
 	while True:
+		if turnCounter%2 == 1:
+			display.playerOneSplash()
+		else:
+			display.playerTwoSplash()
 		# get keyboard input
 		key = keyboardInput.get()
-		print(key)
+		
 		# update data
 		graphicsData = updateData(key)
 		# refresh screen
-		display.drawScreen(graphicsData)
+		display.drawScreen(emptyBoard,emptyBoard)
+		keyboardInput.get()
+
+		turnCounter += 1
--- display.py
+++ display.py
@@ -3,17 +3,41 @@
 def clearScreen():
 	print("\033[1;1H\033[2J\033[3J")
 
-def drawScreen(graphicsData):
-	clearScreen()
+def drawBoard(board):
 	print(" ▁▁▁▁▁▁▁▁▁▁ ")
-	print("▕          ▏")
-	print("▕  ●●●     ▏")
-	print("▕          ▏")
-	print("▕    ●     ▏")
-	print("▕    ●     ▏")
-	print("▕      ●   ▏")
-	print("▕          ▏")
-	print("▕    ● ●   ▏")
-	print("▕          ▏")
-	print("▕          ▏")
-	print(" ▔▔▔▔▔▔▔▔▔▔ ")
+	for row in board:
+		rowDisplayString = "▕"
+		for col in row:
+			if col == 0:
+				rowDisplayString+=" "
+			else:
+				rowDisplayString+="●"
+		rowDisplayString+="▏"
+		print(rowDisplayString)
+	print(" ▔▔▔▔▔▔▔▔▔▔ ")	
+
+def drawScreen(topBoard,bottomBoard):
+	clearScreen()
+	drawBoard(topBoard)
+	drawBoard(bottomBoard)
+
+	# print(" ▁▁▁▁▁▁▁▁▁▁ ")
+	# print("▕          ▏")
+	# print("▕  ●●●●●   ▏")
+	# print("▕        ● ▏")
+	# print("▕    ●   ● ▏")
+	# print("▕    ●     ▏")
+	# print("▕    ●     ▏")
+	# print("▕          ▏")
+	# print("▕    ●●●   ▏")
+	# print("▕          ▏")
+	# print("▕ ●●●●     ▏")
+	# print(" ▔▔▔▔▔▔▔▔▔▔ ")
+
+def playerOneSplash():
+	clearScreen()
+	print("Ready, Player One?")
+
+def playerTwoSplash():
+	clearScreen()
+	print("Ready, Player Two?")
