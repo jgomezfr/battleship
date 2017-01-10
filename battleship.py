e30dbcdfed4761802db365fe4d3b62e2b3318b09
Julieta <julieta.gomez.frittelli@gmail.com>
2017-01-09 17:10:34 -0800

    restrict cursor within board area

--- battleship.py
+++ battleship.py
@@ -13,6 +13,31 @@ playerTwoBottomBoard = emptyBoard
 def updateData(keystroke):
 	pass
 
+def moveTarget(keystroke,cursor):
+	if keystroke == "up":
+		if cursor[0]>0:
+			cursor[0]-=1
+		else:
+			pass
+	elif keystroke == "down":
+		if cursor[0]<9:
+			cursor[0]+=1
+		else:
+			pass
+	elif keystroke == "left":
+		if cursor[1]>0:
+			cursor[1]-=1
+		else:
+			pass
+	elif keystroke == "right":
+		if cursor[1]<9:
+			cursor[1]+=1
+		else:
+			pass
+	else:
+		print("bad keystroke!")
+	return cursor
+
 if __name__ == "__main__":
 	# every loop in main game loop will refresh the screen 
 	# or get keyboard input
@@ -26,11 +51,15 @@ if __name__ == "__main__":
 		# get keyboard input
 		key = keyboardInput.get()
 		target = [4,4]
-		
+		display.drawScreen(emptyBoard,emptyBoard,target)
+		key = keyboardInput.get()
+		while key in ["up","down","left","right"]:
+			target = moveTarget(key,target)
+			display.drawScreen(emptyBoard,emptyBoard,target)
+			key = keyboardInput.get()
 		# update data
 		graphicsData = updateData(key)
 		# refresh screen
-		display.drawScreen(emptyBoard,emptyBoard,target)
 		keyboardInput.get()
 
 		turnCounter += 1
