ca1411c7a5bb525dc576e113ee6f56359929a2bb
Julieta <julieta.gomez.frittelli@gmail.com>
2017-01-09 20:53:34 -0800

    add random piece placement at start of game

--- battleship.py
+++ battleship.py
@@ -3,6 +3,7 @@
 import keyboardInput
 import display
 import time
+import random
 
 # emptyBoard = [ [0] * 10 for _ in range(10)]
 
@@ -10,6 +11,54 @@ playerOneTopBoard = [ [0] * 10 for _ in range(10)]
 playerOneBottomBoard = [ [0] * 10 for _ in range(10)]
 playerTwoTopBoard = [ [0] * 10 for _ in range(10)]
 playerTwoBottomBoard = [ [0] * 10 for _ in range(10)]
+
+def checkCollisions(piece):
+	pass
+
+def addPieceToBoard(board,pieceLocation):
+	for location in pieceLocation:
+		for row in range(len(board)):
+			for col in range(len(board[0])):
+				if location[0]==row and location[1]==col:
+					board[row][col]=1
+				else:
+					pass
+
+def placePieces(playerOne,playerTwo):
+	for player in range(1,3):
+		allShips = ["carrier","battleship","cruiser","submarine","destroyer"]
+		allSizes = [5,4,3,3,2]
+		orientations = ["vertical","horizontal"]
+		for i in range(len(allShips)):
+			shipName = allShips[i]
+			shipSize = allSizes[i]
+			piecePlaced = False
+			while piecePlaced == False:
+				# try to place piece
+				startRow = random.randint(0,9)
+				startCol = random.randint(0,9)
+				orientation = orientations[random.randint(0,1)]
+				pieceLocation = [[startRow,startCol]]
+				currRow = startRow
+				currCol = startCol
+				if orientation == "vertical":
+					for j in range(shipSize-1):
+						currRow+=1
+						pieceLocation.append([currRow,currCol])
+				else: 
+					for j in range(shipSize-1):
+						currCol+=1
+						pieceLocation.append([currRow,currCol])
+				print(pieceLocation)
+				time.sleep(1)
+				# check if piece fits
+				checkCollisions(pieceLocation)
+				# if piece fits (no collisions, on board), add to board
+				if player == 1:
+					addPieceToBoard(playerOne,pieceLocation)
+				else:
+					addPieceToBoard(playerTwo,pieceLocation)
+				piecePlaced = True
 
 def getPlayerTurn(counter):
 	if turnCounter%2 == 1:
@@ -52,6 +101,10 @@ def fireMissile(currboard,aim):
 if __name__ == "__main__":
 	# every loop in main game loop will refresh the screen 
 	# or get keyboard input
+	# randomly place pieces for both players at start of game
+	placePieces(playerOneBottomBoard,playerTwoBottomBoard)
+
+
 	turnCounter = 1
 
 	while True:
